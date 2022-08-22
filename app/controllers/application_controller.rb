f270ff72e405243ee70b61395d1d0a5a1c963889
Flordeliza Mae Canlas <maemaeperlas@gmail.com>
2022-08-21 19:19:19 -0700

    commit tree 30bf2835754af26cbe3ae31297a889af569f94db

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,36 @@
 class ApplicationController < Sinatra::Base
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  # Add this line to se the Content-Type header for all responses
+  set :default_content_type, "application/json"
+
+  get '/games' do
+    # get all the games from the database
+    games = Game.all.order(:title).limit(10) # => [#<Game>, #<Game>, #<Game>]
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+    # use the :id syntax to create a dynamic route
+    get "/games/:id" do
+      # Look up the game in the database using its ID
+      game = Game.find(params[:id])
+      # send a JSON-formatted responses of the game data
+      game.to_json(include: { reviews: { include: :user } })
   end
 
 end
+
+=begin
+  get "/games" do
+    games = Game.all.orders(:title)
+    games.to_json
+  end
+  
+  get "/games" do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
+  end
+
+  only option => be more selective about which attributes are returned from each model
+
+=end
